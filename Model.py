3eba3fc9df8bb1da173e9f6a33c2541332b41b44
Mor Hale <morhale67@gmail.com>
2024-04-29 16:25:52 +0300

    change res block

--- Model.py
+++ Model.py
@@ -43,10 +43,10 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        print(out.shape)
-        print(identity.shape)
         if self.downsample is not None:
             identity = self.downsample(x)
+        print(out.shape)
+        print(identity.shape)
         out += identity
         out = self.relu(out)
         return out
